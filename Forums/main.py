d4cbe619e65c42d1a528051deb5ef862a1b44b4f
khalidhmd <khalidhmd@outlook.com>
2018-02-12 19:36:38 +0200

    Include new folder Forums

---
+++ Forums/main.py
@@ -0,0 +1,7 @@
+import models
+
+member1 = models.Member('Khalid',40)
+member2 = models.Member('Safwat', 43)
+post1 = models.Post('Post1', 'This is the first Post model example')
+post2 = models.Post('Post2', 'Another post example. it is getting exciting')
+post3 = models.Post('Post3', 'Third post. Hopfully i got to the end of this task')
